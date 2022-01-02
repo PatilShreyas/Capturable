3f100de099a211f1c2d795238e96ed28228a6610
shreyaspatilg@gmail.com <shreyaspatilg@gmail.com>
2022-01-02 16:14:19 +0530

    Add initial setup for library

---
+++ app/src/main/java/dev/shreyaspatil/capturableExample/ui/theme/Color.kt
@@ -0,0 +1,8 @@
+package dev.shreyaspatil.capturableExample.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val LightGray = Color(0xFFC2C2C2)
+val Gray = Color(0xFF585858)
+val DarkGray = Color(0xFF242424)
+val Teal200 = Color(0xFF03DAC5)
---
+++ app/src/main/java/dev/shreyaspatil/capturableExample/ui/theme/Theme.kt
@@ -0,0 +1,44 @@
+package dev.shreyaspatil.capturableExample.ui.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+
+private val DarkColorPalette = darkColors(
+    primary = LightGray,
+    primaryVariant = DarkGray,
+    secondary = Teal200
+)
+
+private val LightColorPalette = lightColors(
+    primary = Gray,
+    primaryVariant = DarkGray,
+    secondary = Teal200
+
+    /* Other default colors to override
+    background = Color.White,
+    surface = Color.White,
+    onPrimary = Color.White,
+    onSecondary = Color.Black,
+    onBackground = Color.Black,
+    onSurface = Color.Black,
+    */
+)
+
+@Composable
+fun CapturableExampleTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable() () -> Unit) {
+    val colors = if (darkTheme) {
+        DarkColorPalette
+    } else {
+        LightColorPalette
+    }
+
+    MaterialTheme(
+        colors = colors,
+        typography = Typography,
+        shapes = Shapes,
+        content = content
+    )
+}
