1681836d5dfd05297a40b752053823a9fbef04de
shreyaspatilg@gmail.com <shreyaspatilg@gmail.com>
2022-01-23 12:21:37 +0530

    [#7] Fix capturing bitmap for device with API 26 and above using PixelCopy API

--- capturable/src/main/java/dev/shreyaspatil/capturable/Capturable.kt
+++ capturable/src/main/java/dev/shreyaspatil/capturable/Capturable.kt
@@ -78,8 +78,9 @@ fun Capturable(
     onCaptured: (ImageBitmap?, Throwable?) -> Unit,
     content: @Composable () -> Unit
 ) {
+    val context = LocalContext.current
     AndroidView(
-        factory = { ComposeView(it).applyCapturability(controller, onCaptured, content) },
+        factory = { ComposeView(it).applyCapturability(controller, onCaptured, content, context) },
         modifier = modifier
     )
 }
@@ -90,13 +91,14 @@ fun Capturable(
 private inline fun ComposeView.applyCapturability(
     controller: CaptureController,
     noinline onCaptured: (ImageBitmap?, Throwable?) -> Unit,
-    crossinline content: @Composable () -> Unit
+    crossinline content: @Composable () -> Unit,
+    context: Context
 ) = apply {
     setContent {
         content()
         LaunchedEffect(controller, onCaptured) {
             controller.captureRequests
-                .mapNotNull { config -> drawToBitmapPostLaidOut(config) }
+                .mapNotNull { config -> drawToBitmapPostLaidOut(context, config) }
                 .onEach { bitmap -> onCaptured(bitmap.asImageBitmap(), null) }
                 .catch { error -> onCaptured(null, error) }
                 .launchIn(this)
@@ -107,10 +109,62 @@ private inline fun ComposeView.applyCapturability(
 /**
  * Waits till this [View] is laid off and then draws it to the [Bitmap] with specified [config].
  */
-private suspend fun View.drawToBitmapPostLaidOut(config: Bitmap.Config): Bitmap {
+private suspend fun View.drawToBitmapPostLaidOut(context: Context, config: Bitmap.Config): Bitmap {
     return suspendCoroutine { continuation ->
         doOnLayout { view ->
-            continuation.resume(view.drawToBitmap(config))
+            // For device with API version O(26) and above should draw Bitmap using PixelCopy API.
+            // The reason behind this is it throws IllegalArgumentException saying
+            // "Software rendering doesn't support hardware bitmaps"
+            // See this issue for the reference: https://github.com/PatilShreyas/Capturable/issues/7
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                val window = (context as? Activity)?.window
+                    ?: error("Can't get window from the Context")
+
+                drawBitmapWithPixelCopy(
+                    view = view,
+                    window = window,
+                    config = config,
+                    onDrawn = { bitmap ->  continuation.resume(bitmap) },
+                    onError = { error -> continuation.resumeWithException(error) }
+                )
+            } else {
+                continuation.resume(view.drawToBitmap(config))
+            }
         }
     }
+}
+
+/**
+ * Draws a [view] to a [Bitmap] with [config] using a [PixelCopy] API.
+ * Gives callback [onDrawn] after successfully drawing Bitmap otherwise invokes [onError].
+ */
+@RequiresApi(Build.VERSION_CODES.O)
+private fun drawBitmapWithPixelCopy(
+    view: View,
+    window: Window,
+    config: Bitmap.Config,
+    onDrawn: (Bitmap) -> Unit,
+    onError: (Throwable) -> Unit
+) {
+    val width = view.width
+    val height = view.height
+
+    val bitmap = Bitmap.createBitmap(width, height, config)
+
+    val (x, y) = IntArray(2).apply { view.getLocationInWindow(this) }
+    val rect = Rect(x, y, x + width, y + height)
+
+    PixelCopy.request(
+        window,
+        rect,
+        bitmap,
+        { copyResult ->
+            if (copyResult == PixelCopy.SUCCESS) {
+                onDrawn(bitmap)
+            } else {
+                onError(RuntimeException("Failed to draw bitmap"))
+            }
+        },
+        Handler(Looper.getMainLooper())
+    )
 }
