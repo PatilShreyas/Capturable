e554ad38f00f38c46ed7ff4c64bbf72a24d31195
Shreyas <shreyaspatilg@gmail.com>
2023-10-02 15:24:35 +0530

    #100 | Add async method in capture controller to capture a bitmap

--- capturable/src/main/java/dev/shreyaspatil/capturable/controller/CaptureController.kt
+++ capturable/src/main/java/dev/shreyaspatil/capturable/controller/CaptureController.kt
@@ -26,7 +26,11 @@ package dev.shreyaspatil.capturable.controller
 
 import android.graphics.Bitmap
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.ExperimentalComposeApi
 import androidx.compose.runtime.remember
+import androidx.compose.ui.graphics.ImageBitmap
+import kotlinx.coroutines.CompletableDeferred
+import kotlinx.coroutines.Deferred
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.asSharedFlow
 
@@ -39,7 +43,7 @@ class CaptureController internal constructor() {
     /**
      * Medium for providing capture requests
      */
-    private val _captureRequests = MutableSharedFlow<Bitmap.Config>(extraBufferCapacity = 1)
+    private val _captureRequests = MutableSharedFlow<CaptureRequest>(extraBufferCapacity = 1)
     internal val captureRequests = _captureRequests.asSharedFlow()
 
     /**
@@ -50,9 +54,44 @@ class CaptureController internal constructor() {
      *
      * @param config Bitmap config of the desired bitmap. Defaults to [Bitmap.Config.ARGB_8888]
      */
+    @Suppress("DeferredResultUnused")
+    @OptIn(ExperimentalComposeApi::class)
+    @Deprecated(
+        message = "This method has been deprecated and will be removed in the upcoming releases. " +
+                "Use `captureAsync()` instead",
+        replaceWith = ReplaceWith("captureAsync(config)"),
+        level = DeprecationLevel.WARNING
+    )
     fun capture(config: Bitmap.Config = Bitmap.Config.ARGB_8888) {
-        _captureRequests.tryEmit(config)
+        captureAsync(config)
     }
+
+    /**
+     * Creates and requests for a Bitmap capture with specified [config] and returns
+     * an [ImageBitmap] asynchronously.
+     *
+     * This method is safe to be called from the "main" thread directly.
+     *
+     * Make sure to call this method as a part of callback function and not as a part of the
+     * [Composable] function itself.
+     *
+     * @param config Bitmap config of the desired bitmap. Defaults to [Bitmap.Config.ARGB_8888]
+     */
+    @ExperimentalComposeApi
+    fun captureAsync(config: Bitmap.Config = Bitmap.Config.ARGB_8888): Deferred<ImageBitmap> {
+        val deferredImageBitmap = CompletableDeferred<ImageBitmap>()
+        return deferredImageBitmap.also {
+            _captureRequests.tryEmit(CaptureRequest(imageBitmapDeferred = it, config = config))
+        }
+    }
+
+    /**
+     * Holds information of capture request
+     */
+    internal class CaptureRequest(
+        val imageBitmapDeferred: CompletableDeferred<ImageBitmap>,
+        val config: Bitmap.Config
+    )
 }
 
 /**
