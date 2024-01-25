3f97d9db0c785452f67cc5c792616de8ee5e61dc
Shreyas <shreyaspatilg@gmail.com>
2024-01-25 20:38:56 +0530

    Update docs

--- docs/scripts/sourceset_dependencies.js
+++ docs/scripts/sourceset_dependencies.js
@@ -1,1 +1,1 @@
-sourceset_dependencies='{":capturable:dokkaHtml/androidTestRelease":[],":capturable:dokkaHtml/debug":[],":capturable:dokkaHtml/main":[],":capturable:dokkaHtml/release":[],":capturable:dokkaHtml/testFixtures":[],":capturable:dokkaHtml/testFixturesDebug":[],":capturable:dokkaHtml/testFixturesRelease":[]}'
+sourceset_dependencies='{":capturable:dokkaHtml/debug":[],":capturable:dokkaHtml/main":[],":capturable:dokkaHtml/release":[]}'
